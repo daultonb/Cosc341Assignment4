c1b591d4c4592760c5f2e4459177e145f50bfe66
Daulton Baird <daultonbaird1@gmail.com>
2020-03-13 21:06:24 -0700

    My Profile Activity

---
+++ app/src/main/java/com/example/assignment4/MyProfile.java
@@ -0,0 +1,168 @@
+package com.example.assignment4;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+
+public class MyProfile extends AppCompatActivity {
+    ImageView viewImage;
+    Button b;
+    EditText major;
+    EditText school;
+    EditText hometown;
+    EditText bio;
+    Button btn;
+    boolean editing = false;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        major = findViewById(R.id.major1);
+        school = findViewById(R.id.school1);
+        hometown = findViewById(R.id.hometown1);
+        bio = findViewById(R.id.bio1);
+        btn = findViewById(R.id.editprofile);
+    }
+
+    public void EditProfile(View v){
+        if (!editing){
+            major.setEnabled(true);
+            school.setEnabled(true);
+            hometown.setEnabled(true);
+            bio.setEnabled(true);
+            btn.setText("Submit");
+            editing = true;
+        }
+        else
+        {
+            major.setEnabled(false);
+            school.setEnabled(false);
+            hometown.setEnabled(false);
+            bio.setEnabled(false);
+            btn.setText("Edit Profile");
+            editing = false;
+        }
+
+    }
+
+    public void selectImage(View v) {
+        b = (Button)v;
+        switch(b.getId()){
+            case R.id.btn1:
+                viewImage = findViewById(R.id.pic1);
+                break;
+            case R.id.btn2:
+                viewImage = findViewById(R.id.pic2);
+                break;
+            case R.id.btn3:
+                viewImage = findViewById(R.id.pic3);
+                break;
+            case R.id.btn4:
+                viewImage = findViewById(R.id.pic4);
+                break;
+            case R.id.btn5:
+                viewImage = findViewById(R.id.pic5);
+                break;
+            case R.id.btn6:
+                viewImage = findViewById(R.id.pic6);
+                break;
+            default:
+                viewImage = findViewById(R.id.pic1);
+                break;
+        }
+        final CharSequence[] options = { "Take Photo", "Choose from Gallery","Cancel" };
+        AlertDialog.Builder builder = new AlertDialog.Builder(MyProfile.this);
+        builder.setTitle("Add Photo!");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (options[item].equals("Take Photo"))
+                {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                    startActivityForResult(intent, 1);
+                }
+                else if (options[item].equals("Choose from Gallery"))
+                {
+                    Intent intent = new   Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, 2);
+                }
+                else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                File f = new File(Environment.getExternalStorageDirectory().toString());
+                for (File temp : f.listFiles()) {
+                    if (temp.getName().equals("temp.jpg")) {
+                        f = temp;
+                        break;
+                    }
+                }
+                try {
+                    Bitmap bitmap;
+                    BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+                    bitmap = BitmapFactory.decodeFile(f.getAbsolutePath(),
+                            bitmapOptions);
+                    viewImage.setImageBitmap(bitmap);
+                    String path = android.os.Environment
+                            .getExternalStorageDirectory()
+                            + File.separator
+                            + "Phoenix" + File.separator + "default";
+                    f.delete();
+                    OutputStream outFile = null;
+                    File file = new File(path, String.valueOf(System.currentTimeMillis()) + ".jpg");
+                    try {
+                        outFile = new FileOutputStream(file);
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 85, outFile);
+                        outFile.flush();
+                        outFile.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if (requestCode == 2) {
+                Uri selectedImage = data.getData();
+                String[] filePath = { MediaStore.Images.Media.DATA };
+                Cursor c = getContentResolver().query(selectedImage,filePath, null, null, null);
+                c.moveToFirst();
+                int columnIndex = c.getColumnIndex(filePath[0]);
+                String picturePath = c.getString(columnIndex);
+                c.close();
+                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+                Log.w("path of image from gallery......******************.........", picturePath+"");
+                viewImage.setImageBitmap(thumbnail);
+            }
+        }
+    }
+}
