bf4095fd8151fe086bc6a1abb65305592a008070
Hans <hansfuhrmann870@gmail.com>
2020-03-13 21:30:52 -0700

    Signup/login

---
+++ app/src/main/java/com/example/assignment4/login.java
@@ -0,0 +1,70 @@
+package com.example.assignment4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+    public void back(View view){
+        finish();
+    }
+    public void submit(View view){
+
+        EditText e1 = (EditText)this.findViewById(R.id.editText);
+        EditText e2 = (EditText)this.findViewById(R.id.editText3);
+        String username = e1.getText().toString();
+        String password = e2.getText().toString();
+        String line;
+        try {
+            FileInputStream fis = this.openFileInput("data.txt");
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            line = "";
+
+            while(true) {
+                if ((line = br.readLine()) == null) {
+                    br.close();
+                    isr.close();
+                    fis.close();
+                    break;
+                }
+                String dbusername = line.substring(0, line.indexOf(","));
+                line = line.substring(line.indexOf(",") + 1, line.length());
+                String dbpassword = line.substring(0, line.indexOf(","));
+                if((username.equals(dbusername))&&(password.equals(dbpassword))){
+                    finish();
+                    return;
+                }
+
+
+
+            }
+        } catch (IOException var8) {
+            var8.printStackTrace();
+            Toast.makeText(this.getApplicationContext(), "User not found. Please make sure info is correct.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        Toast.makeText(this.getApplicationContext(), "User not found. Please make sure info is correct.", Toast.LENGTH_SHORT).show();
+        return;
+        //Intent intent = new Intent(this, myprofile.class);
+        //startActivity(intent);
+    }
+}
+
---
+++ app/src/main/java/com/example/assignment4/register.java
@@ -0,0 +1,59 @@
+package com.example.assignment4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileOutputStream;
+
+public class register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void back(View view){
+        finish();
+    }
+    public void submit(View view){
+        EditText e1 = (EditText)this.findViewById(R.id.editText2);
+        EditText e2 = (EditText)this.findViewById(R.id.editText4);
+        EditText e3 = (EditText)this.findViewById(R.id.editText5);
+        EditText e4 = (EditText)this.findViewById(R.id.editText6);
+        EditText e5 = (EditText)this.findViewById(R.id.editText7);
+        EditText e6 = (EditText)this.findViewById(R.id.editText8);
+        String username = e1.getText().toString();
+        String password = e2.getText().toString();
+        String confirm = e3.getText().toString();
+        String hometown = e4.getText().toString();
+        String major = e5.getText().toString();
+        String bio = e6.getText().toString();
+        if (username.length() <1) {
+            Toast.makeText(this.getApplicationContext(), "Please enter a username", Toast.LENGTH_SHORT).show();
+        }else if (!(password.equals(confirm))) {
+            Toast.makeText(this.getApplicationContext(), "Your password must match!", Toast.LENGTH_SHORT).show();
+        }
+        else {
+
+            String writedata = username + "," + password + "," + hometown + "," + major + "," + bio + "\n";
+            String filename = "data.txt";
+
+            try {
+                FileOutputStream outputStream = this.openFileOutput(filename, Context.MODE_APPEND);
+                outputStream.write(writedata.getBytes());
+                outputStream.close();
+            } catch (Exception var17) {
+                var17.printStackTrace();
+            }
+
+            this.finish();
+        }
+
+    }
+}
